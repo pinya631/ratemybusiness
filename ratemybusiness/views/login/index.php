56594a2cb7cee3c86bacfffc6f0da43f43628560
Andrey Alda <andrey.alda13@gmail.com>
2019-01-08 21:33:55 +0800

    Updated the login page with remember me and a sign up page.

--- ratemybusiness/views/login/index.php
+++ ratemybusiness/views/login/index.php
@@ -14,43 +14,52 @@
 	<header class="alt">
 		<div class = "container">
 			<div class="row">
-				<div class="col-md-4 offset-md-4 box-type">
+				<div class="col-sm-10 col-md-6 col-lg-6 offset-sm-1 offset-md-3 offset-lg-3 ">
 					<div class="logo">
 						<a href="http://localhost/ratemybusiness/">
 						<img src = "http://localhost/ratemybusiness/assets/images/rate-my-business.png" width=300 alt="rate my business">
 						</a>					
+						<p class="alternative-font-style text-center ">Customer Review Management System</p>
 					</div>
 				</div>
 			</div>
 		</div>
 	</header>
-<section class='alt'>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4 offset-md-4 box-type">
-				<div class="error"><p><?php (isset($err_msg) ? print $err_msg : "");  ?></p></div>
-				<?php echo form_open('login'); ?>
+	<section class='alt'>
+		<div class="container">
+			<div class="row">
+					<div class="col-sm-10 col-md-6 col-lg-6 offset-sm-1 offset-md-3 offset-lg-3 box-type">
+					<div class="error"><p><?php (isset($err_msg) ? print $err_msg : "");  ?></p></div>
+					<?php echo form_open('login'); ?>
 
-					<label for="title">Email</label>
-					<input type="email" name="email" />
-					<div class ="error-msg"><?php echo form_error('email'); ?></div>
+						<input type="email" name="email" placeholder="Email" />
+						<div class ="error-msg"><?php echo form_error('email'); ?></div>
 
-					<label for="title">Password</label>
-					<input type="password" name="password" />
-					<div class ="error-msg"><?php echo form_error('password'); ?></div>
-
-					<input type="submit" name="submit" value="Login" />
-
-				</form>
-			</div>
-	</div>
-	</div>
-</section>
+						<input type="password" name="password" placeholder="Password" />
+						<div class ="error-msg"><?php echo form_error('password'); ?></div>
+						
+						<div class ="row">
+							<div class="col-6 ">
+								<div class="custom-checkbox">
+									<label class="check">Remember me?</label>
+									<input type="checkbox" id="remember">
+								</div>
+							</div>
+							<div class="col-6 ">
+								<input type="submit" name="submit" value="Login" />
+								<button type="button" onclick="location.href='https://www.facebook.com'" name="signup" >Signup</button>
+							</div>
+						</div>
+					</form>
+				</div>
+		</div>
+		</div>
+	</section>
 	<footer  class="alt">
 		<div class = "container">
 				<div class="row">
-					<div class="col-md-4 offset-md-4 box-type">
-						<p class="copyright">Page rendered in <strong>0.0315</strong> seconds. CodeIgniter Version <strong>3.1.9</strong></p>
+					<div class="col-sm-10 col-md-6 col-lg-6 offset-sm-1 offset-md-3 offset-lg-3 box-type">
+						<p class="copyright text-center">Page rendered in <strong>0.0315</strong> seconds. CodeIgniter Version <strong>3.1.9</strong></p>
 					</div>
 				</div>
 		</div>
